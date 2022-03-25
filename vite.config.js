46411764167effa820a5e526893fbff9c5f67643
sean-kintone <seanluse@cybozu.onmicrosoft.com>
2022-03-25 18:04:53 +0900

    Vite + TypeScript + Kintone-Customize-Uploader

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import path from 'path'
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/main.ts')
+      },
+      output: {
+        entryFileNames: `KintoneCustomization.js`,
+        assetFileNames: `[name].[ext]`,
+        dir: './dist'
+      },
+    }
+  }
+}
